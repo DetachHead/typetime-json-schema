946c3115b306e9b4c9866c25230ae7766046fecb
DetachHead <detachhead@users.noreply.github.com>
2021-09-30 23:53:23 +1000

    support enums, tuples and boolean schemas

--- src/__tests__/index.spec.ts
+++ src/__tests__/index.spec.ts
@@ -1,40 +1,63 @@
 import { exactly } from '@detachhead/ts-helpers/dist/utilityFunctions/misc'
 import { FromJsonSchema } from '../index'
 
-test('basic', () => {
-    exactly<
-        { foo: string },
-        FromJsonSchema<{ type: 'object'; properties: { foo: { type: 'string' } } }>
-    >()
+describe('boolean schemas', () => {
+    test('true schema resolves to unknown', () => {
+        exactly<unknown, FromJsonSchema<true>>()
+    })
+    test('false schema resolves to never', () => {
+        exactly<never, FromJsonSchema<false>>()
+    })
 })
 
-test('object with array', () => {
-    exactly<
-        { foo: { asdf: string }; bar: string[] },
-        FromJsonSchema<{
-            type: 'object'
-            properties: {
-                foo: {
-                    type: 'object'
-                    properties: {
-                        asdf: {
+describe('objects', () => {
+    test('object with additionalProperties', () => {
+        exactly<
+            { foo: string; [key: string]: unknown },
+            FromJsonSchema<{
+                type: 'object'
+                properties: { foo: { type: 'string' } }
+                additionalProperties: true
+            }>
+        >()
+    })
+    test('object without additionalProperties', () => {
+        exactly<
+            { foo: string },
+            FromJsonSchema<{
+                type: 'object'
+                properties: { foo: { type: 'string' } }
+                additionalProperties: false
+            }>
+        >()
+    })
+
+    test('object with array', () => {
+        exactly<
+            { foo: { asdf: string }; bar: string[] },
+            FromJsonSchema<{
+                type: 'object'
+                properties: {
+                    foo: {
+                        type: 'object'
+                        properties: {
+                            asdf: {
+                                type: 'string'
+                            }
+                        }
+                        additionalProperties: false
+                    }
+                    bar: {
+                        type: 'array'
+                        items: {
                             type: 'string'
                         }
                     }
                 }
-                bar: {
-                    type: 'array'
-                    items: {
-                        type: 'string'
-                    }
-                }
-            }
-        }>
-    >()
-})
-
-test('union of valid types (types is an array)', () => {
-    exactly<string | number, FromJsonSchema<{ type: ['string', 'number'] }>>()
+                additionalProperties: false
+            }>
+        >()
+    })
 })
 
 describe('arrays', () => {
@@ -46,5 +69,28 @@ describe('arrays', () => {
                 items: [{ type: 'string' }, { type: 'number' }]
             }>
         >()
+    })
+    test('tuple type without additionalItems', () => {
+        exactly<
+            [string, number],
+            FromJsonSchema<{
+                type: 'array'
+                items: [{ type: 'string' }, { type: 'number' }]
+                additionalItems: false
+            }>
+        >()
+    })
+})
+
+test('union of valid types (types is an array)', () => {
+    exactly<string | number, FromJsonSchema<{ type: ['string', 'number'] }>>()
+})
+
+describe('enums', () => {
+    test('basic', () => {
+        exactly<'foo' | 'bar', FromJsonSchema<{ enum: ['foo', 'bar'] }>>()
+    })
+    test("enum types don't match type (never)", () => {
+        exactly<never, FromJsonSchema<{ enum: ['foo', 'bar']; type: 'number' }>>()
     })
 })
--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,85 @@
-import { JSONSchema6 } from 'json-schema'
+import { JSONSchema7, JSONSchema7Definition, JSONSchema7Type } from 'json-schema'
 import { TODO } from '@detachhead/ts-helpers/dist/utilityTypes/misc'
 import { CastArray } from '@detachhead/ts-helpers/dist/utilityTypes/Array'
 import { Head } from 'ts-toolbelt/out/List/Head'
 import { Tail } from 'ts-toolbelt/out/List/Tail'
 
-type FromJsonSchemaArray<T extends JSONSchema6[]> = T['length'] extends 0
+type FromJsonSchemaArray<T extends JSONSchema7[]> = T['length'] extends 0
     ? []
     : [FromJsonSchema<Head<T>>, ...FromJsonSchemaArray<Tail<T>>]
 
-export type FromJsonSchema<T extends JSONSchema6> = T['type'] extends {}
-    ? {
-          string: string
-          number: number
-          integer: number
-          boolean: boolean
-          null: null
-          array: T['items'] extends JSONSchema6
-              ? FromJsonSchema<T['items']>[]
-              : [
-                    ...(T['items'] extends JSONSchema6[]
-                        ? FromJsonSchemaArray<T['items']>
-                        : unknown[]),
-                    ...(T['additionalItems'] extends false ? [] : unknown[])
-                ]
-          object: undefined extends T['properties']
-              ? {}
-              : {
-                    [K in keyof T['properties']]: T['properties'][K] extends JSONSchema6
-                        ? FromJsonSchema<T['properties'][K]>
-                        : TODO<'object type - why are arrays/booleans allowed here'>
-                }
-          any: unknown // any? more like unknown
-      }[CastArray<T['type']>[number]]
-    : TODO<'unknown type'>
+/**
+ * converts a JSON schema to a typescript type at compiletime
+ *
+ * unlike other JSON schema to type converters, this one doesn't use any code generation.
+ * simply pass this type a {@link JSONSchema7Definition} and the compiler will convert it to a typescript type for you
+ *
+ * **note:** this won't work when importing a schema from a JSON file. if you want that functionality,
+ * updoot [this issue](https://github.com/microsoft/TypeScript/issues/32063)
+ * @example
+ * type Foo = FromJsonSchema<{
+ *    type: 'object'
+ *    properties: {
+ *       foo: {
+ *           type: 'object'
+ *           properties: {
+ *               asdf: {
+ *                   type: 'string'
+ *               }
+ *           }
+ *           additionalProperties: false
+ *       }
+ *       bar: {
+ *           type: 'array'
+ *           items: {
+ *               type: 'string'
+ *           }
+ *       }
+ *   }
+ *   additionalProperties: false
+ * }>
+ *
+ * // Type '{}' is missing the following properties from type '{ foo: { asdf: string; }; bar: string[]; }': foo, bar
+ * const foo: Foo = {}
+ */
+export type FromJsonSchema<T extends JSONSchema7Definition> = T extends JSONSchema7
+    ? (T['type'] extends {}
+          ? // @ts-expect-error https://github.com/microsoft/TypeScript/issues/46145
+            {
+                string: string
+                number: number
+                integer: number
+                boolean: boolean
+                null: null
+                array: T['items'] extends JSONSchema7
+                    ? FromJsonSchema<
+                          // @ts-expect-error see issue linked above
+                          T['items']
+                      >[]
+                    : [
+                          ...(T['items'] extends JSONSchema7[]
+                              ? FromJsonSchemaArray<
+                                    // @ts-expect-error see issue linked above
+                                    T['items']
+                                >
+                              : unknown[]),
+                          ...(T['additionalItems'] extends false ? [] : unknown[])
+                      ]
+                object: (undefined extends T['properties']
+                    ? {}
+                    : {
+                          [K in keyof T['properties']]: T['properties'][K] extends JSONSchema7
+                              ? FromJsonSchema<T['properties'][K]>
+                              : TODO<'object type - why are arrays/booleans allowed here'>
+                      }) &
+                    (T['additionalProperties'] extends false ? {} : Record<string, unknown>)
+                any: unknown // any? more like unknown
+            }[CastArray<T['type']>[number]]
+          : unknown) &
+          (T['enum'] extends JSONSchema7Type[]
+              ? // @ts-expect-error see issue linked above
+                T['enum'][number]
+              : unknown)
+    : T extends true
+    ? unknown
+    : never
